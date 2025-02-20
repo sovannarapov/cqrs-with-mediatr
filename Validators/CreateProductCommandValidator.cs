f6fb863a803ea01585999b7311efdfc2f73c3ee7
sovannarapov <sovannarapov9@gmail.com>
2025-02-20 14:41:00 +0700

    refactor: update CreateProductCommandValidator

--- Validators/CreateProductCommandValidator.cs
+++ Validators/CreateProductCommandValidator.cs
@@ -3,9 +3,9 @@ using CQRSWithMediatR.Commands;
 
 namespace CQRSWithMediatR.Validators;
 
-public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+public abstract class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
 {
-    public CreateProductCommandValidator()
+    protected CreateProductCommandValidator()
     {
         RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
         RuleFor(x => x.Price).GreaterThan(0);
